4509a731956ba4d8a047d03be78f31d5903b62a6
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2024-01-30 11:29:57 +0200

    add

--- templates_advanced/templates_advanced/settings.py
+++ templates_advanced/templates_advanced/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "templates_advanced.web",
 ]
 
 MIDDLEWARE = [
@@ -102,6 +103,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+STATICFILES_DIRS = (
+    BASE_DIR / 'staticfiles',
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
---
+++ templates_advanced/templates_advanced/web/views.py
@@ -0,0 +1,30 @@
+import random
+
+from django.shortcuts import render
+
+cat_images = (
+    'https://th.bing.com/th/id/R.41ec5c9ca2e897c49a0ab68e333da8e8?rik=dsYPYiGysjyoNA&riu=http%3a%2f%2fthewowstyle.com%2fwp-content%2fuploads%2f2015%2f04%2fcat1.jpg&ehk=GTCh9L6QOJd5t7FK13U8fi4c4z%2bqjD05seLLFGiHa1g%3d&risl=&pid=ImgRaw&r=0',
+    'https://th.bing.com/th/id/R.7ceb81f6b2c615743919109333a742d5?rik=4SXH%2bKdUQ%2fhIbQ&riu=http%3a%2f%2fimages2.fanpop.com%2fimage%2fphotos%2f9400000%2fFunny-Cats-cats-9473111-1600-1200.jpg&ehk=zpmRdnX5thsU88ZKDfowPaEEzpGU3oUdfHeu3d05ZNA%3d&risl=&pid=ImgRaw&r=0',
+)
+
+cat_names = (
+    'Pepelyashka',
+    'Gosho',
+)
+
+
+def index(request):
+    index = random.randint(0, len(cat_names) - 1)
+    # To give us index of each object, so we can take its photo and name
+    context = {
+        'cat_image': cat_images[index],
+        'cat_name': cat_names[index],
+
+        # for our custom filter example:
+        'numbers': [x + 1 for x in range(-10, 10)],
+    }
+    return render(request, 'web/index.html', context)
+
+
+def about(request):
+    return render(request, 'web/about.html')
