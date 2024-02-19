74c1443d2ab5c536211c5a8f74b0e91744c3d05a
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2024-02-19 22:41:01 +0200

    add

---
+++ exam_music_app/exam_music_app/web/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+from exam_music_app.profiles.models import Profile
+
+
+""" 
+We create get_profile function in order to check if there is a profile created.
+We need this verification for index view.
+We gave the function Profile.objects.first() to check if there is any profile or not.
+"""
+
+
+def get_profile():
+    return Profile.objects.first()
+
+
+def create_profile(request):  # we can use this function or just return this in index view
+    return render(request, 'web/home-no-profile.html')
+
+
+def index(request):
+    profile = get_profile()
+
+    if profile is None:
+        return create_profile(request)
+
+    return render(request, 'web/home-with-profile.html')
