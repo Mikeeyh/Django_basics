5e22fe9c784dbd86b64a3943cc91336e977ff1fa
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2024-02-19 20:50:24 +0200

    add

---
+++ exam_prep_my_music_app/exam_prep_my_music_app/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # Adding our app`s paths:
+    path("", include('exam_prep_my_music_app.web.urls')),
+    path("", include('exam_prep_my_music_app.albums.urls')),
+    path("", include('exam_prep_my_music_app.profiles.urls')),
+]
